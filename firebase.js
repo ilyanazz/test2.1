aa020775a8c3ec637f1f47ccaeed82f76b4a4ef2
ilyanazz <arifairiyana27@gmail.com>
2025-05-06 11:48:42 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDaRLmnE2t8v_bXtm_r_RrnuMhtJBfomug",
-    authDomain: "test2-74b30.firebaseapp.com",
-    projectId: "test2-74b30",
-    storageBucket: "test2-74b30.firebasestorage.app",
-    messagingSenderId: "776427276353",
-    appId: "1:776427276353:web:0c7285a031ff3271069ec0"
-  };
+  apiKey: "AIzaSyDaRLmnE2t8v_bXtm_r_RrnuMhtJBfomug",
+  authDomain: "test2-74b30.firebaseapp.com",
+  databaseURL: "https://test2-74b30-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test2-74b30",
+  storageBucket: "test2-74b30.firebasestorage.app",
+  messagingSenderId: "776427276353",
+  appId: "1:776427276353:web:0c7285a031ff3271069ec0"
+};
   
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
